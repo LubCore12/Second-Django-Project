0da056ba6d58b0b5c4f26218143f867fcb13e4fb
Andrew Fomin <andrfom1234567810@gmail.com>
2026-03-08 15:02:45 +0700

    Basic urls added

--- women-site/women-site/wsgi.py
+++ women-site/women-site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for women-app project.
+WSGI config for women-site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'women-app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'women-site.settings')
 
 application = get_wsgi_application()
