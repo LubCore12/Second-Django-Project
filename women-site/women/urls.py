358ed41a3bac532a2a5d422b9d82bbd0547bd4a2
Andrew Fomin <andrfom1234567810@gmail.com>
2026-03-08 16:14:34 +0700

    Custom converters and more urls added

--- women-site/women/urls.py
+++ women-site/women/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from women import views
+from django.urls import path, register_converter
+from . import views
+from . import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index),
-    path('cats/', views.categories)
+    path('archive/<year4:year>', views.categories_by_year),
+    path('cats/<slug:cat_id>/', views.categories_by_id),
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),
 ]
--- women-site/women/views.py
+++ women-site/women/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories_by_id(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по айди</h1><p>id: {cat_id}</p>")
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+def categories_by_year(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>year: {year}</p>")
