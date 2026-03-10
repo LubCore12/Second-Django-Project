d372d2ae0b3bfb48ff4b59c0cb8eb2d353a083df
Andrew Fomin <andrfom1234567810@gmail.com>
2026-03-10 07:09:39 +0700

    Extendable templates added

--- women-site/women/urls.py
+++ women-site/women/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, register_converter
 from . import views
-from . import converters
-
-register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('about', views.about, name='about'),
-    path('archive/<year4:year>', views.categories_by_year, name='archive'),
-    path('cats/<int:cat_id>', views.categories_by_id, name='cats_id'),
-    path('cats/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
+    path('about/', views.about, name='about'),
+    path('addpage/', views.addpage, name='add_page'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
+    path('post/<int:post_id>/', views.show_post, name='post'),
 ]
